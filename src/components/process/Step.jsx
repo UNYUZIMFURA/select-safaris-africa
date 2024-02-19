3fae77229bf9718178949934df0b31a5dc83813d
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-19 16:34:32 +0200

    Feat: Finished clients' reviews section

--- src/components/process/Step.jsx
+++ src/components/process/Step.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Step = (props) => {
   return (
-    <div className="w-full md:h-[16rem] flex flex-col gap-5 pt-6 pb-8 px-4 text-white bg-[#ffffff18] rounded-[10px] cursor-pointer">
+    <div className="w-full  md:h-[16rem] flex flex-col gap-5 pt-6 pb-8 px-4 text-white bg-[#ffffff18] rounded-[10px] cursor-pointer">
       <div className="h-[3.5rem] w-[3.5rem] flex items-center justify-center text-[#10A969] rounded-full bg-[#73954318]">
         <span className="font-bold">{props.no}</span>
       </div>
--- src/components/reviews/Review.jsx
+++ src/components/reviews/Review.jsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import { FaRegStar } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 
 const Review = () => {
   return (
     <div className="relative flex flex-col items-center bg-white px-4 pb-[3rem] pt-[5.5rem] rounded-3xl">
-        <div className="h-[7.5rem] w-[7.5rem] flex items-center justify-center bg-white absolute -top-14 rounded-full p-5">
-            <div className="h-full w-full border border-black rounded-full cursor-pointer"></div>
+      <div className="h-[7.5rem] w-[7.5rem] flex items-center justify-center bg-white absolute -top-14 rounded-full p-4">
+        <div className="relative h-full w-full rounded-full cursor-pointer">
+          <Image
+            src="/images/client.jpg"
+            alt=""
+            fill={true}
+            className="object-cover rounded-full"
+          />
         </div>
+      </div>
       <div className="flex flex-col gap-6 text-center items-center">
         <h2 className="font-bold text-[22px] md:text-[24px]">Jeff Davis</h2>
         <span className="text-[#AAB2BA] leading-[28px] sm:max-w-[28rem] font-light">
