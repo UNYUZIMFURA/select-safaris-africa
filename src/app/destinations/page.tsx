9658af971768a80ecf4569b510cc714112d6e012
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-22 21:58:40 +0200

    Feat: Almost done with the Destinations Page

--- src/app/destinations/page.tsx
+++ src/app/destinations/page.tsx
@@ -2,9 +2,29 @@ import Image from "next/image";
 import Header from "@/components/header/Header";
 import Travel from "@/components/travels/Travel";
 import { BiSearch } from "react-icons/bi";
-import "@/components/travels/styles/travels.css"
+import "@/components/travels/styles/travels.css";
+import "@/components/footer/Footer";
+import Footer from "@/components/footer/Footer";
 
 const Destinations = () => {
+  const travelCategories = [
+    {
+      title: "Game Parks",
+      image: "lion",
+    },
+    {
+      title: "Restaurants and Resorts",
+      image: "restaurant",
+    },
+    {
+      title: "Lakes and rivers",
+      image: "lake",
+    },
+    {
+      title: "Hotels and Motels",
+      image: "hotel",
+    },
+  ];
   return (
     <div className="flex flex-col w-full text-white">
       <div
@@ -17,12 +37,7 @@ const Destinations = () => {
         <Header />
         <div className="flex flex-col gap-3 items-center">
           <h2 className="text-4xl font-bold">Destinations</h2>
-          <Image
-            src="/images/underline.png"
-            alt=""
-            height={1}
-            width={210}
-          />
+          <Image src="/images/underline.png" alt="" height={1} width={210} />
         </div>
         <div className="flex w-full flex-wrap justify-center gap-3">
           <button className="outline-none py-4 px-8 rounded-full bg-theme3">
@@ -44,20 +59,20 @@ const Destinations = () => {
               className="text-tertiary2 pl-7 rounded-l-full h-full w-[75%] outline-none"
               placeholder="Where to?"
             />
-            <div className="h-4/5 bg-brand px-6 md:px-8 gap-6 rounded-full flex items-center justify-center">
+            <div className="cursor-pointer h-4/5 bg-brand px-6 md:px-8 gap-6 rounded-full flex items-center justify-center">
               <span className="hidden md:flex">Search</span>
               <BiSearch size={20} className="cursor-pointer" />
             </div>
           </div>
         </form>
       </div>
-      <div className="flex flex-col bg-white py-[5rem] px-4 lg:py-[8rem] 2xl:px-[8rem] min-[1200px]:px-[3rem]">
-        <div className="flex flex-col gap-4">
+      <div className="flex flex-col bg-white py-[5rem] px-4 lg:py-[8rem] 2xl:px-[8rem] gap-6">
+        <div className="flex flex-col gap-4 pl-2">
           <span className="text-brand font-bold">TRAVELS</span>
-          <h2 className="font-bold text-tertiary2 text-[24px]">
+          <h2 className="font-bold text-tertiary2 text-[24px] lg:text-[28px]">
             Suggested for you
           </h2>
-          <span className="text-lg text-secondary">
+          <span className="text-lg text-secondary max-w-[34rem]">
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
             odio. Quisque volutpat mattis eros.
           </span>
@@ -69,6 +84,36 @@ const Destinations = () => {
           <Travel img="zebra" name="Akagera Park" />
         </div>
       </div>
+      <div className="flex flex-col bg-white pb-8 px-4 2xl:px-[8rem] gap-6">
+        <div className="flex flex-col gap-4 pl-2">
+          <span className="text-brand font-bold">CATEGORIES</span>
+          <h2 className="font-bold text-tertiary2 text-[24px] lg:text-[28px]">
+            Choose by Type
+          </h2>
+          <span className="text-lg text-secondary max-w-[34rem]">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
+            odio. Quisque volutpat mattis eros.
+          </span>
+        </div>
+        <div className="travels">
+          {travelCategories.map((el, index) => (
+            <div className="w-full relative cursor-pointer" key={index}>
+              <div className="overflow-hidden relative h-[22rem] border rounded-xl">
+                <Image
+                  src={`/images/${el.image}.webp`}
+                  alt=""
+                  fill={true}
+                  className="object-cover brightness-75 transition hover:brightness-100"
+                />
+              </div>
+              <span className="text-tertiary1 font-bold text-tertiary absolute bottom-6 left-6">
+                {el.title}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 };
