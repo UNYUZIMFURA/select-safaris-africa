f18b2462f77f216bbb2f8ad01411e4a82e8c5b11
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-29 21:11:51 +0200

    Style: Still working on the About Us section

--- src/components/about/Objective.tsx
+++ src/components/about/Objective.tsx
@@ -12,15 +12,15 @@ const Objective: React.FC<Props> = ({Icon, title}) => {
     return (
       <div className="shadow-lg w-full flex flex-col gap-5 py-12 px-3 text-tertiary2 bg-white items-center rounded-[10px]">
         <div className="flex flex-col items-center gap-4">
-          <div className="flex items-center justify-center h-[3.5rem] w-[3.5rem] rounded-[18px] bg-[#E7F6F3]">
+          <div className="cursor-pointer flex items-center justify-center h-[3.5rem] w-[3.5rem] rounded-[18px] bg-[#E7F6F3]">
             <Icon
               fill="#10A969"
               size={20}
             />
           </div>
-          <h3 className="text-xl font-bold">{title}</h3>
+          <h3 className="text-xl font-bold md:text-2xl">{title}</h3>
         </div>
-        <p className="text-center text-secondary max-w-[30rem] 2xl:px-10 leading-[2rem]">
+        <p className="text-center text-secondary max-w-[33rem] md:text-[1.2rem] 2xl:px-10 leading-[2rem] min-[880px]:text-[16px]">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
           odio.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec
           odio.
