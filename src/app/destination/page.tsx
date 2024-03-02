65aca4b8f523a184a4d26c7efbe545690f35cd82
UNYUZIMFURA <banjoflylive@gmail.com>
2024-03-02 02:47:08 +0200

    Feat: Banner image change onhover

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,9 +1,12 @@
+"use client";
 import Image from "next/image";
 import Header from "@/components/header/Header";
+import { useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { RiArrowRightSLine } from "react-icons/ri";
 
 const Destination = () => {
+  const [hoveredImg, setHoveredImg] = useState<string>("banner-1");
   return (
     <div className="max-w-[125rem] w-full flex flex-col">
       <Header />
@@ -28,7 +31,8 @@ const Destination = () => {
         </div>
         <div className="flex lg:gap-8 h-[25rem] md:h-[28rem] xl:h-[32rem] md:-bottom-[18rem] absolute w-full px-4 min-[1200px]:px-14 -bottom-[20rem] left-0 right-0 mx-auto 2xl:px-[8rem]">
           <div
-            className="cursor-pointer w-full flex flex-col px-4 pb-4 gap-4 justify-end rounded-lg h-full bg-cover md:px-8 md:pb-8 lg:w-[60%]"
+            onMouseOver={() => setHoveredImg("banner-1")}
+            className={`cursor-pointer w-full flex flex-col px-4 pb-4 gap-4 justify-end rounded-lg h-full bg-cover md:px-8 md:pb-8 ${hoveredImg == "banner-1" ? "lg:w-[60%]" : "lg:w-[20%]"} `}
             style={{
               backgroundImage:
                 "linear-gradient(to top, rgba(0,0,0,0.7), transparent),url('/images/car.webp')",
@@ -41,7 +45,11 @@ const Destination = () => {
               Lorem ipsum dolor sit amet, consectetuer adipiscing elit
             </span>
           </div>
-          <div className="cursor-pointer transition relative brightness-75 hover:brightness-100 hidden lg:flex h-full w-[20%] rounded-[20px] overflow-hidden">
+          <div
+            className={`cursor-pointer relative brightness-75 hidden lg:flex h-full rounded-[20px] overflow-hidden ${hoveredImg == "banner-2" ? "lg:w-[60%]" : "lg:w-[20%]"}`}
+            onMouseOver={(e) => setHoveredImg("banner-2")}
+            onMouseLeave={(e) => setHoveredImg("banner-1")}
+          >
             <Image
               src="/images/mountains.webp"
               fill
@@ -49,7 +57,11 @@ const Destination = () => {
               className="object-cover"
             />
           </div>
-          <div className="cursor-pointer transition relative brightness-75 hover:brightness-100 overflow-hidden hidden lg:flex h-full w-[20%] rounded-[20px]">
+          <div
+            className={`cursor-pointer relative brightness-75 overflow-hidden hidden lg:flex h-full w-[20%] rounded-[20px] ${hoveredImg == "banner-3" ? "lg:w-[60%]" : "lg:w-[20%]"}`}
+            onMouseOver={(e) => setHoveredImg("banner-3")}
+            onMouseLeave={(e) => setHoveredImg("banner-1")}
+          >
             <Image
               src="/images/about.webp"
               fill
