f767ebdfc02b778f746b15e7722858778f778c07
UNYUZIMFURA <banjoflylive@gmail.com>
2024-03-04 17:10:14 +0200

    Style: Working on the bookings page

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -5,25 +5,27 @@ import Booking from "@/components/bookings/Booking";
 
 const Page = () => {
   return (
-    <div className="w-full flex flex-col">
-      <Header />
-      <div
-        className="relative py-[6rem] md:py-[8rem] lg:py-[9rem] xl:py-[10rem] w-full bg-slate-200 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            "linear-gradient(to right, rgba(0,0,0,0.8), transparent),url('/images/mountain.webp')",
-        }}
-      ></div>
-      <TopSection />
-      <div className="flex flex-col py-[7rem] md:py-[4rem] md:ml-[17rem] px-4 min-[1200px]:px-[4rem] lg:ml-[20rem] xl:ml-[23rem] 2xl:px-[8.5rem]">
-        <span className="text-white text-xl font-bold">Your bookings</span>
-        <div className="flex flex-col gap-4 py-4">
-          <Booking />
-          <Booking />
-          <Booking />
+    <div className="w-full">
+      <div className="bg-[#F3F8F5] w-full flex flex-col">
+        <Header />
+        <div
+          className="relative py-[6rem] md:py-[8rem] lg:py-[9rem] xl:py-[10rem] w-full bg-cover bg-center"
+          style={{
+            backgroundImage:
+              "linear-gradient(to right, rgba(0,0,0,0.8), transparent),url('/images/mountain.webp')",
+          }}
+        ></div>
+        <TopSection />
+        <div className="flex flex-col py-[7rem] md:py-[4rem] md:ml-[17rem] px-4 min-[1200px]:px-[4rem] lg:ml-[20rem] xl:ml-[23rem] 2xl:px-[8.5rem]">
+          <span className="text-white text-xl font-bold">Your bookings</span>
+          <div className="flex flex-col gap-4 py-4">
+            <Booking />
+            <Booking />
+            <Booking />
+          </div>
         </div>
       </div>
-      {/* <Footer />   */}
+      <Footer />
     </div>
   );
 };
