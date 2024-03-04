894c9b35d6814e4909e0584fe49728ee7327058d
UNYUZIMFURA <banjoflylive@gmail.com>
2024-03-04 10:41:19 +0200

    Add: Working on bookings page

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -2,6 +2,7 @@ import { MdLibraryBooks } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
+import Image from "next/image";
 
 const Page = () => {
   return (
@@ -17,7 +18,9 @@ const Page = () => {
       <div className="w-full relative flex flex-col">
         <div className="w-full flex-col min-[500px]:px-4 min-[500px]:gap-6 md:gap-8 absolute -bottom-[13rem] sm:-bottom-[6.5rem] md:-bottom-[8rem] lg:-bottom-[8.5rem] xl:-bottom-[9rem] lg:gap-[3rem] min-[300px]:items-center min-[300px]:-bottom-[5rem] min-[500px]:-bottom-[5.5rem] px-2 min-[300px]:flex-row flex py-2 gap-2 min-[1200px]:px-[4rem] 2xl:px-[8.5rem]">
           <div className="bg-white rounded-md py-4 px-2 min-[500px]:px-4 md:p-10 lg:px-14 xl:px-[5rem] border flex flex-col items-center justify-center gap-4">
-            <div className="h-[3rem] w-[3rem] sm:h-[5rem] sm:w-[5rem] xl:h-[7rem] xl:w-[7rem] rounded-full relative overflow-hidden border"></div>
+            <div className="h-[3rem] w-[3rem] sm:h-[5rem] sm:w-[5rem] xl:h-[7rem] xl:w-[7rem] rounded-full relative overflow-hidden border cursor-pointer">
+              <Image fill src="/images/client.webp" alt="" className="object-cover object-top"/>
+            </div>
             <span className="text-xs min-[350px]:text-sm sm:text-base lg:text-lg font-semibold">John Ivan Doe</span>
             <span className="text-xs min-[350px]:text-sm text-secondary">johnivan130@gmail.com</span>
           </div>
