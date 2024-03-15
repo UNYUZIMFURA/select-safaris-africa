0bf35cf355e8870056fae65e4197c7d57ac4f937
UNYUZIMFURA <banjoflylive@gmail.com>
2024-03-15 09:27:19 +0200

    Add: Re-add images on Destination Page

--- src/components/destination/DestinationBanner.tsx
+++ src/components/destination/DestinationBanner.tsx
@@ -29,7 +29,7 @@ const DestinationBanner = () => {
       <div className="flex lg:gap-8 h-[25rem] md:h-[28rem] xl:h-[32rem] md:-bottom-[18rem] absolute w-full px-3 min-[1200px]:px-14 -bottom-[20rem] left-0 right-0 mx-auto 2xl:px-[8rem]">
         <div
           onMouseOver={() => setHoveredImg("banner-1")}
-          className={`load-light cursor-pointer w-full flex flex-col px-4 pb-4 gap-4 justify-end rounded-lg h-full bg-cover md:px-8 md:pb-8 ${hoveredImg == "banner-1" ? "lg:w-[60%]" : "lg:w-[20%]"} `}
+          className={`load-light-2 cursor-pointer w-full flex flex-col px-4 pb-4 gap-4 justify-end rounded-lg h-full bg-cover md:px-8 md:pb-8 ${hoveredImg == "banner-1" ? "lg:w-[60%]" : "lg:w-[20%]"} `}
           style={{
             backgroundImage:
               "linear-gradient(to top, rgba(0,0,0,0.7), transparent),url('/images/car.webp')",
@@ -46,15 +46,15 @@ const DestinationBanner = () => {
           className={`load-light-2 cursor-pointer relative brightness-75 hidden lg:flex h-full rounded-[20px] overflow-hidden ${hoveredImg == "banner-2" ? "lg:w-[60%]" : "lg:w-[20%]"}`}
           onMouseOver={() => setHoveredImg("banner-2")}
         >
-          {/* <Image
+          <Image
             src="/images/mountains.webp"
             fill
             alt=""
             className="object-cover"
-          /> */}
+          />
         </div>
         <div
-          className={`load-light cursor-pointer relative brightness-75 overflow-hidden hidden lg:flex h-full w-[20%] rounded-[20px] ${hoveredImg == "banner-3" ? "lg:w-[60%]" : "lg:w-[20%]"}`}
+          className={`load-light-2 cursor-pointer relative brightness-75 overflow-hidden hidden lg:flex h-full w-[20%] rounded-[20px] ${hoveredImg == "banner-3" ? "lg:w-[60%]" : "lg:w-[20%]"}`}
           onMouseOver={() => setHoveredImg("banner-3")}
         >
           <Image
