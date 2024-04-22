44ed4bceb288fcfa36729cc97ece6a9be1ce1329
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-22 20:29:26 +0200

    Style: Adjust padding on the bottom of the Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -27,7 +27,7 @@ const Footer = () => {
         },
     ]
     return (
-        <div className="flex flex-col gap-10 bg-[#19211A] px-5 py-8 text-tertiary1 min-[600px]:px-14 md:justify-between md:gap-12 lg:flex-row lg:py-[4rem]  2xl:px-[8rem]">
+        <div className="flex flex-col gap-10 bg-[#19211A] px-5 pt-8 pb-[5rem] text-tertiary1 min-[600px]:px-14 md:justify-between md:gap-12 lg:flex-row lg:py-[4rem] 2xl:px-[8rem]">
             <BackToTop />
             <div className="flex flex-col gap-8 md:flex-row md:justify-between lg:w-[calc(100%-20rem)] 2xl:w-[calc(90%-20rem)]">
                 <div className="flex flex-col gap-6">
