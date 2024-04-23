4da4a55d75722b9f2f9fa42a38391064caf010fd
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-23 23:45:14 +0200

    Style: Adjust dimensions of Destination Card

--- src/components/travels/Travel.tsx
+++ src/components/travels/Travel.tsx
@@ -15,13 +15,13 @@ interface Props {
 const Travel: React.FC<Props> = ({name, image, amount, location, description}) => {
     const router = useRouter()
     return (
-        <div className="flex w-full flex-col items-center gap-4 rounded-[20px] p-5 shadow-lg xl:max-w-[380px]">
+        <div className="flex h-[37rem] w-full flex-col items-center gap-4 rounded-[20px] p-5 shadow-lg xl:max-w-[380px]">
             <div className="load-dark relative h-[20rem] w-full cursor-pointer overflow-hidden rounded-[20px] min-[600px]:h-[22rem] min-[700px]:h-[20rem]">
                 <Image
                     src={`/images/${image}.webp`}
                     alt=""
                     fill={true}
-                    className="object-cover"
+                    className="object-cover transition 2xl:brightness-75 2xl:hover:brightness-100"
                 />
             </div>
             <div className="flex w-full flex-col gap-4 px-1 pt-4 sm:px-2">
