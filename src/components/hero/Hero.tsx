cabac60e3b638ae2ff37034bcc11b2b29b178f23
UNYUZIMFURA <banjoflylive@gmail.com>
2024-05-03 10:10:37 +0200

    Add: Change content on Hero

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -74,9 +74,9 @@ const Hero = () => {
                     SELECT SAFARIS AFRICA
                 </h1>
                 <p className="max-w-[50rem] text-2xl font-bold leading-10 min-[370px]:text-[30px] min-[370px]:leading-[3.5rem] min-[400px]:text-[36px] min-[700px]:text-[2.8rem] min-[700px]:leading-[4.2rem] min-[1200px]:text-[33px] min-[1200px]:leading-[3.3rem] 2xl:text-[56px] 2xl:leading-[5rem]">
-                    People don't take
+                    People don't take 
                     <span className="inline-block bg-gradient-to-r from-white via-[rgb(93,189,183)] to-[rgb(85,183,103)] bg-clip-text text-transparent">
-                        trips
+                        trips,
                     </span>{" "}
                     trips take people
                 </p>
